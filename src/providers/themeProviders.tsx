774b8e5058b29b074fa85e2c30b57bf321b8c6a5
suyeong.jeong <suyeong.jeong@navercorp.com>
2024-09-15 21:45:35 +0900

    init

---
+++ src/providers/themeProviders.tsx
@@ -0,0 +1,6 @@
+import { ThemeProviderProps } from "next-themes/dist/types";
+import { ThemeProvider as NextThemeProvider } from "next-themes";
+
+export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
+  return <NextThemeProvider {...props}>{children}</NextThemeProvider>;
+}
